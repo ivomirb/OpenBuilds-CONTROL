a3aae363e4a98df5c6fe36839519e59effc5e576
ivomirb <85206280+ivomirb@users.noreply.github.com>
2026-09-18 09:22:11 -0700

    Graphics fixes
    * Fixed orbiting controls - uses Z as up vector
    * Changed inch grid to 10 divisions (since the move steps are 10-based)

--- app/js/viewer-ruler.js
+++ app/js/viewer-ruler.js
@@ -1,17 +1,18 @@
-function drawRuler(xmin, xmax, ymin, ymax) {
+function drawRuler(xmin, xmax, ymin, ymax, inches) {
+  const unitsVal = inches ? '"' : '';
+  const ticSpacing = inches ? 2.54 : 1;
+	const unitScale = inches ? 0.1 : 1; // convert tic index to text
+
   const length1 = 4;
   const length5 = 6;
   const length10 = 7;
-  const unitsval = "";
   const fontsize = 4;
-  const spacing = -10;
+  const ticOffset = -10;
 
-  // console.log(xmin, xmax, ymin, ymax)
-
-  xmin = Math.ceil(xmin);
-  xmax = Math.floor(xmax);
-  ymin = Math.ceil(ymin);
-  ymax = Math.floor(ymax);
+  xmin = Math.ceil(xmin / ticSpacing);
+  xmax = Math.floor(xmax / ticSpacing);
+  ymin = Math.ceil(ymin / ticSpacing);
+  ymax = Math.floor(ymax / ticSpacing);
 
   var ruler = new THREE.Group();
 
@@ -20,14 +21,14 @@ function drawRuler(xmin, xmax, ymin, ymax) {
   for (var i = xmin; i <= xmax; i++) {
     var length = (i % 10 == 0) ? length10 : ((i % 5 == 0) ? length5 : length1);
     var geometry = new THREE.Geometry();
-    vertices.push(i, -1, 0, i, -length, 0);
+    vertices.push(i * ticSpacing, -1, 0, i * ticSpacing, -length, 0);
 
     if (i % 10 == 0) {
       var sprite = makeSprite("webgl", {
-        x: i,
-        y: spacing,
+        x: i * ticSpacing,
+        y: ticOffset,
         z: 0,
-        text: i + unitsval,
+        text: Math.round(i * unitScale) + unitsVal,
         color: Theme.X_RULER_NUMBER_COLOR,
         size: fontsize
       });
@@ -39,14 +40,14 @@ function drawRuler(xmin, xmax, ymin, ymax) {
   for (var i = ymin; i <= ymax; i++) {
     var length = (i % 10 == 0) ? length10 : ((i % 5 == 0) ? length5 : length1);
     var geometry = new THREE.Geometry();
-    vertices.push(-1, i, 0, -length, i, 0);
+    vertices.push(-1, i * ticSpacing, 0, -length, i * ticSpacing, 0);
 
     if (i % 10 == 0) {
       var sprite = makeSprite("webgl", {
-        x: spacing,
-        y: i,
+        x: ticOffset,
+        y: i * ticSpacing,
         z: 0,
-        text: i + unitsval,
+        text: Math.round(i * unitScale) + unitsVal,
         color: Theme.Y_RULER_NUMBER_COLOR,
         size: fontsize
       });
@@ -68,22 +69,22 @@ function drawRuler(xmin, xmax, ymin, ymax) {
 
   return ruler;
 }
-
+/* inch version that uses 1/16 spacing instead of 1/10
 function drawRulerInches(xmin, xmax, ymin, ymax) {
   const length1 = 4;
   const length4 = 6;
   const length16 = 7;
-  const unitsval = "in";
+  const unitsVal = "in";
   const fontsize = 4;
-  const spacing = -10;
+  const ticSpacing = -10;
 
   // console.log(xmin, xmax, ymin, ymax)
 
-  const scale = 25.4 / 16; // 1/16th of an inch
-  xmin = Math.ceil(xmin / scale);
-  xmax = Math.floor(xmax / scale);
-  ymin = Math.ceil(ymin / scale);
-  ymax = Math.floor(ymax / scale);
+  const unitScale = 25.4 / 16; // 1/16th of an inch
+  xmin = Math.ceil(xmin / unitScale);
+  xmax = Math.floor(xmax / unitScale);
+  ymin = Math.ceil(ymin / unitScale);
+  ymax = Math.floor(ymax / unitScale);
 
   var ruler = new THREE.Group();
 
@@ -92,14 +93,14 @@ function drawRulerInches(xmin, xmax, ymin, ymax) {
   for (var i = xmin; i <= xmax; i++) {
     var length = (i % 16 == 0) ? length16 : ((i % 4 == 0) ? length4 : length1);
     var geometry = new THREE.Geometry();
-    vertices.push(i * scale, -1, 0, i * scale, -length, 0);
+    vertices.push(i * unitScale, -1, 0, i * unitScale, -length, 0);
 
     if (i % 16 == 0) {
       var sprite = makeSprite("webgl", {
-        x: i * scale,
-        y: spacing,
+        x: i * unitScale,
+        y: ticSpacing,
         z: 0,
-        text: (i/16) + unitsval,
+        text: (i/16) + unitsVal,
         color: Theme.X_RULER_NUMBER_COLOR,
         size: fontsize
       });
@@ -111,14 +112,14 @@ function drawRulerInches(xmin, xmax, ymin, ymax) {
   for (var i = ymin; i <= ymax; i++) {
     var length = (i % 16 == 0) ? length16 : ((i % 4 == 0) ? length4 : length1);
     var geometry = new THREE.Geometry();
-    vertices.push(-1, i * scale, 0, -length, i * scale, 0);
+    vertices.push(-1, i * unitScale, 0, -length, i * unitScale, 0);
 
     if (i % 16 == 0) {
       var sprite = makeSprite("webgl", {
-        x: spacing,
-        y: i * scale,
+        x: ticSpacing,
+        y: i * unitScale,
         z: 0,
-        text: (i/16) + unitsval,
+        text: (i/16) + unitsVal,
         color: Theme.Y_RULER_NUMBER_COLOR,
         size: fontsize
       });
@@ -140,3 +141,4 @@ function drawRulerInches(xmin, xmax, ymin, ymax) {
 
   return ruler;
 }
+*/
